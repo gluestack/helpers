96ff71863cf09398cf1bfe660304e75294fc1d53
Shivam Singhal <shivams@geekyants.com>
2022-12-21 17:43:24 +0530

    v0.0.5

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,12 @@
 "use strict";
 exports.__esModule = true;
-exports.SpawnHelper = exports.NodemonHelper = exports.DockerodeHelper = void 0;
+exports.GlobalEnv = exports.SpawnHelper = exports.NodemonHelper = exports.DockerodeHelper = void 0;
 var Dockerode_1 = require("./helpers/Dockerode");
 exports.DockerodeHelper = Dockerode_1.DockerodeHelper;
 var Nodemon_1 = require("./helpers/Nodemon");
 exports.NodemonHelper = Nodemon_1.NodemonHelper;
 var Spawn_1 = require("./helpers/Spawn");
 exports.SpawnHelper = Spawn_1.SpawnHelper;
+var GlobalEnv_1 = require("./helpers/GlobalEnv");
+exports.GlobalEnv = GlobalEnv_1.GlobalEnv;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { DockerodeHelper } from "./helpers/Dockerode";
 import { NodemonHelper } from "./helpers/Nodemon";
 import { SpawnHelper } from "./helpers/Spawn";
+import { GlobalEnv } from "./helpers/GlobalEnv";
 
-export { DockerodeHelper, NodemonHelper, SpawnHelper };
+export { DockerodeHelper, NodemonHelper, SpawnHelper, GlobalEnv };
