ab5b7835cff6edc013b2bc895d84ac042d6692f9
Syed Yaseen Abbas Rizvi <yaseenr@geekyants.com>
2023-02-15 12:48:11 +0530

    chore: added common helpers

---
+++ dist/helpers/remove-special-chars.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.removeSpecialChars = void 0;
+var removeSpecialChars = function (str) {
+    return str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+};
+exports.removeSpecialChars = removeSpecialChars;
+//# sourceMappingURL=remove-special-chars.js.map
