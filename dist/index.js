fedbb1d50f3c0e65c02fa0d955ce7fb093ad7c90
Syed Yaseen Abbas Rizvi <yaseenr@geekyants.com>
2023-02-15 12:15:54 +0530

    chore: added console table

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-exports.GlobalEnv = exports.SpawnHelper = exports.NodemonHelper = exports.DockerodeHelper = void 0;
+exports.ConsoleTable = exports.GlobalEnv = exports.SpawnHelper = exports.NodemonHelper = exports.DockerodeHelper = void 0;
 var Dockerode_1 = require("./helpers/Dockerode");
 exports.DockerodeHelper = Dockerode_1.DockerodeHelper;
 var Nodemon_1 = require("./helpers/Nodemon");
@@ -9,4 +9,6 @@ var Spawn_1 = require("./helpers/Spawn");
 exports.SpawnHelper = Spawn_1.SpawnHelper;
 var GlobalEnv_1 = require("./helpers/GlobalEnv");
 exports.GlobalEnv = GlobalEnv_1.GlobalEnv;
+var ConsoleTable_1 = require("./helpers/ConsoleTable");
+exports.ConsoleTable = ConsoleTable_1.ConsoleTable;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { DockerodeHelper } from "./helpers/Dockerode";
 import { NodemonHelper } from "./helpers/Nodemon";
 import { SpawnHelper } from "./helpers/Spawn";
 import { GlobalEnv } from "./helpers/GlobalEnv";
+import { ConsoleTable } from "./helpers/ConsoleTable";
 
-export { DockerodeHelper, NodemonHelper, SpawnHelper, GlobalEnv };
+export { DockerodeHelper, NodemonHelper, SpawnHelper, GlobalEnv, ConsoleTable };
