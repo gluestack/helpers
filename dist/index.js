6811e6b1fe0b46cc147ad2773ea17d25b5dc8f15
Syed Yaseen Abbas Rizvi <yaseenr@geekyants.com>
2023-02-15 13:15:53 +0530

    chore: added common helpers

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,47 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
 exports.__esModule = true;
-exports.ConsoleTable = exports.GlobalEnv = exports.SpawnHelper = exports.NodemonHelper = exports.DockerodeHelper = void 0;
-var Dockerode_1 = require("./helpers/Dockerode");
-exports.DockerodeHelper = Dockerode_1.DockerodeHelper;
-var Nodemon_1 = require("./helpers/Nodemon");
-exports.NodemonHelper = Nodemon_1.NodemonHelper;
+exports.writeContentToFilePath = exports.waitInSeconds = exports.writeFile = exports.getOSFolders = exports.removeSpecialChars = exports.getFiles = exports.getDirectories = exports.fileExists = exports.createFolder = exports.addTrailingSlash = exports.Workspaces = exports.ConsoleTable = exports.DockerodeHelper = exports.NodemonHelper = exports.GlobalEnv = exports.SpawnHelper = void 0;
 var Spawn_1 = require("./helpers/Spawn");
-exports.SpawnHelper = Spawn_1.SpawnHelper;
+__createBinding(exports, Spawn_1, "SpawnHelper");
 var GlobalEnv_1 = require("./helpers/GlobalEnv");
-exports.GlobalEnv = GlobalEnv_1.GlobalEnv;
+__createBinding(exports, GlobalEnv_1, "GlobalEnv");
+var Nodemon_1 = require("./helpers/Nodemon");
+__createBinding(exports, Nodemon_1, "NodemonHelper");
+var Dockerode_1 = require("./helpers/Dockerode");
+__createBinding(exports, Dockerode_1, "DockerodeHelper");
 var ConsoleTable_1 = require("./helpers/ConsoleTable");
-exports.ConsoleTable = ConsoleTable_1.ConsoleTable;
+__createBinding(exports, ConsoleTable_1, "ConsoleTable");
+var Workspaces_1 = require("./helpers/Workspaces");
+__createBinding(exports, Workspaces_1, "Workspaces");
+var add_trailing_slash_1 = require("./helpers/add-trailing-slash");
+__createBinding(exports, add_trailing_slash_1, "addTrailingSlash");
+var create_folder_1 = require("./helpers/create-folder");
+__createBinding(exports, create_folder_1, "createFolder");
+var file_exists_1 = require("./helpers/file-exists");
+__createBinding(exports, file_exists_1, "fileExists");
+var get_directories_1 = require("./helpers/get-directories");
+__createBinding(exports, get_directories_1, "getDirectories");
+var get_files_1 = require("./helpers/get-files");
+__createBinding(exports, get_files_1, "getFiles");
+var remove_special_chars_1 = require("./helpers/remove-special-chars");
+__createBinding(exports, remove_special_chars_1, "removeSpecialChars");
+var get_os_folders_1 = require("./helpers/get-os-folders");
+__createBinding(exports, get_os_folders_1, "getOSFolders");
+var write_file_1 = require("./helpers/write-file");
+__createBinding(exports, write_file_1, "writeFile");
+var wait_in_seconds_1 = require("./helpers/wait-in-seconds");
+__createBinding(exports, wait_in_seconds_1, "waitInSeconds");
+var write_content_to_filepath_1 = require("./helpers/write-content-to-filepath");
+__createBinding(exports, write_content_to_filepath_1, "writeContentToFilePath");
 //# sourceMappingURL=index.js.map
