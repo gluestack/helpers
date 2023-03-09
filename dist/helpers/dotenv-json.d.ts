0275244dd7535ae19dad6f30ca08df5d72936d41
Shivam Singhal <shivams@geekyants.com>
2023-03-09 14:47:11 +0530

    v0.0.14

---
+++ dist/helpers/dotenv-json.d.ts
@@ -0,0 +1,2 @@
+export declare function envToJson(path: string): Promise<any>;
+export declare function jsonToEnv(envConfig: any): string;
---
+++ dist/helpers/get-cross-env-key.d.ts
@@ -0,0 +1,2 @@
+export declare const getCrossEnvKey: (instance: string, key: string) => string;
+export declare const getPrefix: (instance: string) => string;
